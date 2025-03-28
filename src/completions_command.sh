cadcd37faf57d1d348095e0178d628c43d85b721
Adam MKHININI <adam.mkh@hotmail.fr>
2025-03-28 13:32:44 +0100

    Restored completion

--- src/completions_command.sh
+++ src/completions_command.sh
@@ -1,3 +1,4 @@
 echo "# this file is located in 'src/completions_command.sh'"
 echo "# code for 'kryctl completions' goes here"
 echo "# you can edit it freely and regenerate (it will not be overwritten)"
+send_completions
